90b865242394edddd0001f69c72adc386bca32c3
Shekib <vhsu30@yahoo.com>
2017-05-19 19:45:44 +0430

    remove two || and use indexOf for acction_flag_for_remove

--- src/drr.d
+++ src/drr.d
@@ -149,7 +149,7 @@ int main( immutable string[] args ){
 
 	immutable clone_index = index;
 	immutable bool action_flag_for_rename = ( action_flag.indexOf( "s" ) != -1 && number_of_delimiter >= 3 );
-	immutable bool action_flag_for_remove = ( action_flag == "d" || action_flag == "rd" || action_flag == "dr");
+	immutable bool action_flag_for_remove = action_flag.indexOf( "d" ) != -1;
 	immutable bool end_flag_has_g = end_flag.indexOf( "g" ) != -1;
 
 	/*********************************************************************/
